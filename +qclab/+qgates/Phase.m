aa17461c83146a222a81552e37bbfe1d92ca0a9a
Simone Ianniciello <iannisimo@gmail.com>
2026-05-06 12:22:32 +0200

    fix: typo

--- +qclab/+qgates/Phase.m
+++ +qclab/+qgates/Phase.m
@@ -146,7 +146,7 @@ classdef Phase < qclab.qgates.QGate1 & qclab.QAdjustable
         mat = [1, 0;
                0, p];
       else 
-        % TODO isSparse dependant on d
+        % TODO isSparse dependent on d
         isSparse = false;
         mat = qclab.qId(1, isSparse, d);
         mat(d, d) = p;
