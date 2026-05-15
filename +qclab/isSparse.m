3b78248b1b822f9eea6d8a9bdc57aa54e5d0dfbb
Simone Ianniciello <iannisimo@gmail.com>
2026-05-15 13:38:52 +0200

    fix(isSparse): result depends on d

--- +qclab/isSparse.m
+++ +qclab/isSparse.m
@@ -8,11 +8,16 @@
 %
 % (C) Copyright Daan Camps, Sophia Keip and Roel Van Beeumen 2025.  
 % ==============================================================================
-function [isSparse] = isSparse(nbQubits)
-if nbQubits < 9
-  isSparse = false ;
-else
-  isSparse = true ;
-end
+function [isSparse] = isSparse(nbQubits, d)
+  if nargin <= 1, d = 2; end
+  if d == 2
+    if nbQubits < 9
+      isSparse = false ;
+    else
+      isSparse = true ;
+    end
+  else
+    isSparse = d^nbQubits < 512;
+  end
 end
 
